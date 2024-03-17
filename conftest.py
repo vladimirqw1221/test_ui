22ede0b3cf214bc42c2df7b835dd77f939931c1a
Vladimir <111503066+vladimirqw1221@users.noreply.github.com>
2024-03-18 00:23:47 +0400

    new options

--- conftest.py
+++ conftest.py
@@ -9,6 +9,8 @@ def driver(request):
     options.add_argument('--window-size=1800,1020')
     options.add_argument('no-sandbox')
     options.add_argument('--headless')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage ")
     driver = webdriver.Chrome(options=options)
     request.cls.driver = driver
 
