475918d0dd7d710d82bef6d44ed4f6878e1a5dd0
vladimirqw1221 <vladimirqw1221@users.noreply.github.com>
2024-03-18 13:27:01 +0000

    Deploying to gh-pages from @ vladimirqw1221/test_ui@6a79beb367fd13212d86cc0e942d1f73f04442ab 🚀

--- data_project/fake_data.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class DataPerson:
-    first_name: str = None
-    last_name: str = None
-    post_code: str = None
--- global_enums/enums_halpper/global_enums.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum
-
-
-class GlobalEnums(Enum):
-    WRONG_ERROR_TITLE = "This title is not equal your value"
